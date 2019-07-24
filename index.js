507cef457d1cb5079ff33cfd40933d368c25bf0b
Paul <paulerogerson@gmail.com>
2019-07-24 09:05:24 +0100

    implement strangle and add busy to constructor

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 class PsychicOcto {
   constructor() {
     this.laughing = true;
+    this.busy = false;
   }
   giggle() {
     console.log('rofl');
+  }
+  strangle() {
+    this.busy = true;
   }
 }
